36f08d8a3f1782632558c2e898ff9ba5a2d6539a
psychexp <rohitbokade94@gmail.com>
2024-06-02 12:56:02 -0400

    update trips

--- pytsc/tests/test_cityflow_flow_generation.py
+++ pytsc/tests/test_cityflow_flow_generation.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     # Generate hourly flows
     flow_resolution = 25
-    for size in [4]:
+    for size in [2]:
         for replicate_no in range(1, 21):
             for mean_flow in [600]:
                 trip_generator = CityFlowTripGenerator(
