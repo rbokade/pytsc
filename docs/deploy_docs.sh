4aa47ca118e04f8732b523e649b30010d680d7f6
psychexp <rohitbokade94@gmail.com>
2025-04-23 13:14:27 -0400

    added documentation

---
+++ docs/deploy_docs.sh
@@ -0,0 +1,25 @@
+# docs/deploy_docs.sh
+
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Build docs
+cd docs
+make html
+
+# Move to build output
+cd _build/html
+
+# Init new git repo inside build dir
+git init
+git add .
+git commit -m "Deploy docs"
+
+# Push to gh-pages branch
+git push --force git@github.com:rbokade/pytsc.git master:gh-pages
+
+# Cleanup
+cd ../..
+rm -rf docs/_build/html/.git
