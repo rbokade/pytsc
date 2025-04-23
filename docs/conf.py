26d5055f595a4f8207b6310e6c472c9128384adf
psychexp <rohitbokade94@gmail.com>
2025-04-23 18:54:05 -0400

    updated documentation

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,7 @@ extensions = [
     'myst_parser',
     'sphinx_rtd_theme',
     'sphinx.ext.autodoc',
+    'sphinxcontrib.bibtex',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
     'sphinx.ext.autosummary'
@@ -54,7 +55,7 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+bibtex_bibfiles = ['references.bib'] 
 
 # -- Options for HTML output -------------------------------------------------
 
